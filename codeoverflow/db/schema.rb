38de6487c9eb6bef36bcf436c2e0ed6b9f0169b3
solomonfy <fantahunsolomon@gmail.com>
2020-09-24 18:12:04 -0400

    rolledback migration

--- codeoverflow/db/schema.rb
+++ codeoverflow/db/schema.rb
@@ -10,44 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_204816) do
+ActiveRecord::Schema.define(version: 0) do
 
-  create_table "answers", force: :cascade do |t|
-    t.text "body"
-    t.string "solution_type"
-    t.integer "solution_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["solution_type", "solution_id"], name: "index_answers_on_solution_type_and_solution_id"
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.text "title"
-    t.text "body"
-    t.string "tag"
-    t.integer "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_questions_on_user_id"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password_digest"
-    t.string "image"
-    t.integer "company_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["company_id"], name: "index_users_on_company_id"
-  end
-
-  add_foreign_key "questions", "users"
-  add_foreign_key "users", "companies"
 end
